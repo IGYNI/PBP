23f2091bb152c8c5792bcf465ee65dbe590a0362
YaYana-K <yanulja17@gmail.com>
2024-01-22 18:24:04 +0200

    Main menu scene

---
+++ PBP/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] GameObject optionsPanel;
+
+    private void Start()
+    {
+        optionsPanel.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+}
