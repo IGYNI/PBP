7d78ca51e64bf8eaee5901c7b470595ff52ff180
Torren <maaxxx.123123@gmail.com>
2024-01-25 11:16:34 +0200

    Update Scene 4
    Fixed some bugs with main scene 4 gameplay

--- PBP/Assets/Scripts/Scene4/GameDistance.cs
+++ PBP/Assets/Scripts/Scene4/GameDistance.cs
@@ -6,12 +6,17 @@ public class GameDistance : MonoBehaviour
     [SerializeField] private Transform maxPosition;
     [SerializeField] private Transform minPosition;
     [HideInInspector] public float distance; //distance between player and maxPosition
+    [HideInInspector] private float playedDistance; //distance between minPosition and maxPosition
 
+    private void Start()
+    {
+        playedDistance = Vector3.Distance(minPosition.position, maxPosition.position);
+    }
     private void Update()
     {
         distance = Vector3.Distance(player.transform.position, maxPosition.position);
 
-        if (player.transform.position == minPosition.position)
+        if (distance >= playedDistance)
         {
             PauseGame();
         }
